82625018a67140a2ca4033994043d5ff0f6cfd8d
Arvind Kumar <arvindkumar.rules@gmail.com>
2012-09-10 14:43:00 +0530

    Added Speed factor.

--- Char.java
+++ Char.java
@@ -2,15 +2,19 @@ public class Char{
     String image;
 	int x=0;
 	int y=0;
+	int speed=1;
     
     public Char(String url){
         image=url;
     }
     
     public void changeX(int gx){
-        x+=gx;
+        x+=(gx*speed);
     }
     public void changeY(int gy){
-        y+=gy;
+        y+=(gy*speed);
+    }
+    public void changeSpeed(int s){
+        speed=s;
     }
 }
